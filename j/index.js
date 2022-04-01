c98ba1f8d3b58383c21ac34daf0b797106c0b406
housia <97599314+housia@users.noreply.github.com>
2022-04-01 15:59:25 +0530

    Add files via upload

---
+++ j/index.js
@@ -0,0 +1,12 @@
+const addClickEvent = () => {
+    document.addEventListener('click', () => {
+        const video = document.querySelector('video');
+        const videoMessage = document.getElementById('video-message');
+  
+        video.play();
+        document.body.removeChild(videoMessage);
+    });  
+  };
+  window.addEventListener('load', addClickEvent);
+  
+  
